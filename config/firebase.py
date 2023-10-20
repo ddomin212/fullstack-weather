c25debd938fbe99901542de4ec2831842b918577
Daniel Dominko <danieldominko@gmail.com>
2023-10-20 19:34:32 +0200

    missing file fix

---
+++ config/firebase.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+import pyrebase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = os.getenv("FIREBASE_CONFIG")
+
+firebase = pyrebase.initialize_app(json.loads(config))
+auth = firebase.auth()
+db = firebase.database()
